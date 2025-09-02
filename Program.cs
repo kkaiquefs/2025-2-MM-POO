074e7e670ae362d4d13a8438d6d3e8996ff7094d
kkaiquefs <130922872+kkaiquefs@users.noreply.github.com>
2025-09-02 00:31:56 +0000

    feat:

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-    Televisao tv = new Televisao();
-    
-    tv.Tamanho = 22.5f;
+    Televisao tv = new Televisao(55f);
 
     Console.WriteLine($"A TV tem o tamanho {tv.Tamanho}");
+
+    tv.Volume = -35;
+    Console.WriteLine($"Volume {tv.Volume}");
+
+    tv.Volume = 120;
+    Console.WriteLine($"Volume {tv.Volume}");
--- Televisao.cs
+++ Televisao.cs
@@ -1,8 +1,27 @@
 public class Televisao
 {
-    public float Tamanho {get; set;}
+
+    public Televisao(float tamanho)
+    {
+        Tamanho = tamanho;
+    }
+
+    public float Tamanho {get;}
     public int Resolucao {get; set;}
     public int Volume {get; set;}
     public int Canal {get; set;}
     public bool Estado {get; set;}
+
+    public void AumentarVolume(){
+
+        Volume = Volume + 1;
+
+    }
+
+    public void DiminuirVolume(){
+
+        Volume = Volume - 1;
+
+    }
+
 }
