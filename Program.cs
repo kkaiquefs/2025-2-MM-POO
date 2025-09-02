d71a44025a0ba239b7438873aadb371eb7f2f0f8
kkaiquefs <130922872+kkaiquefs@users.noreply.github.com>
2025-09-02 00:01:15 +0000

    feat:

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+    Televisao tv = new Televisao();
+    
+    tv.Tamanho = 22.5f;
+
+    Console.WriteLine($"A TV tem o tamanho {tv.Tamanho}");
